e571c358aa526c00c776899c82480432ebc6e22d
Orianne Garaud <104376376+ogaraud@users.noreply.github.com>
2022-10-17 15:47:14 +0200

    modif vignette cartel

--- WEB-INF/classes/fr/digiwin/module/EspaceEnseignant/EspaceEnseignantHandler.java
+++ WEB-INF/classes/fr/digiwin/module/EspaceEnseignant/EspaceEnseignantHandler.java
@@ -2,6 +2,8 @@ package fr.digiwin.module.EspaceEnseignant;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.jalios.jcms.Category;
 import com.jalios.jcms.Channel;
@@ -41,6 +43,35 @@ public class EspaceEnseignantHandler {
         return listPeriode;
     }
 
+    public static Map<Category, Category> getPeriodeCategory(Member loggedMember, Fiche fiche) {
+        Map<Category, Category> periodes = new HashMap<>();
+        if (Util.notEmpty(fiche.getPeriode(loggedMember))) {  
+            String catId = channel.getProperty("jcmsplugin.espaceEnseignant.espace.cat.root");
+            if(Util.notEmpty(catId)) {
+                // cherche période principale de la fiche
+                Category catPeriode = channel.getCategory(catId); //cat de la catégorie période
+                for(Category itCat : fiche.getPeriode(loggedMember)) { //liste des catégories de la fiche
+                    boolean isParent = false;
+                    Category catParent = itCat.getParent();
+                    while(!isParent) {
+                        if(Util.notEmpty(catParent.getParent()) && catParent.getParent().getId().equals(catPeriode.getId())) {
+                            periodes.put(catParent, itCat);
+                            isParent = true; //fin on trouve le parent
+                        } else {
+                            if(Util.notEmpty(catParent.getParent())) {
+                                catParent = catParent.getParent(); //prochain parent
+                            } else {
+                                isParent = true; //sinon fin
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return periodes;
+    }
+
+
     public static ArrayList<Category> getTypologieList(Member loggedMember, Fiche fiche) { 
         ArrayList<Category> listTypo = new ArrayList<Category>();
         if (Util.notEmpty(fiche.getTypologie(loggedMember))) {  
@@ -62,7 +93,9 @@ public class EspaceEnseignantHandler {
     public static boolean isCartel(String userLang, Fiche fiche) {
     	return Util.notEmpty(fiche.getTitreCartel(userLang)) ||
     			Util.notEmpty(fiche.getNomDeLartiste(userLang)) ||
+    			Util.notEmpty(fiche.getDateCartel(userLang)) ||
     			Util.notEmpty(fiche.getMatiere(userLang)) ||
+    			Util.notEmpty(fiche.getDimension(userLang)) ||
     			Util.notEmpty(fiche.getLieu(userLang)) ||
     			Util.notEmpty(fiche.getLegs(userLang)) ||
     			Util.notEmpty(fiche.getNinventaire(userLang)) ||
